f07d4d137bad4679f0245321c6ff3cf1c069558b
zihan <zihanhannah@gmail.com>
2020-08-18 10:21:39 -0500

    inheritance and polymorphism

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -3,16 +3,16 @@ package shapes;
 public class ShapesTest {
     public static void main(String[] args) {
         Rectangle box1 = new Rectangle(4,5);
-        System.out.println(box1.getPerimeter());
-        System.out.println(box1.getArea());
+        System.out.println("Perimeter of the box1: " + box1.getPerimeter());
+        System.out.println("Area of the box1: " + box1.getArea());
 
         Rectangle box2 = new Square(5);
-        System.out.println(box2.getPerimeter());
-        System.out.println(box2.getArea());
+        System.out.println("Perimeter of the box2: " + box2.getPerimeter());
+        System.out.println("Area of the box2: " + box2.getArea());
 
-        Square box3 = new Square(5);
-        System.out.println(box3.getPerimeter());
-        System.out.println(box3.getArea());
+//        Square box3 = new Square(5);
+//        System.out.println(box3.getPerimeter());
+//        System.out.println(box3.getArea());
 
 
 
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,17 +1,19 @@
 package shapes;
 
 public class Square extends Rectangle {
-    protected int side;
+    private int side;
+
     public Square (int side){
         super(side,side);
+        this.side = side;//important
     }
 
-//    public int getArea(){
-//        return side * side;
-//    }
-//    public int getPerimeter(){
-//        return 4*side;
-//    }
+    public int getArea(){
+        return this.side * this.side;
+    }
+    public int getPerimeter(){
+        return 4*this.side;
+    }
 
 
 
