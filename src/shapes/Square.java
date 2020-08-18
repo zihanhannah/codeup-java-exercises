7543d3176208300919a7d95264cd50a6ed364f06
zihan <zihanhannah@gmail.com>
2020-08-18 00:06:25 -0500

    inheritance

---
+++ src/shapes/Square.java
@@ -0,0 +1,18 @@
+package shapes;
+
+public class Square extends Rectangle {
+    protected int side;
+    public Square (int side){
+        super(side,side);
+    }
+
+//    public int getArea(){
+//        return side * side;
+//    }
+//    public int getPerimeter(){
+//        return 4*side;
+//    }
+
+
+
+}
