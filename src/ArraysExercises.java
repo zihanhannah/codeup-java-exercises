50a926d3189da14f9716f4949450156383a670b8
zihan <zihanhannah@gmail.com>
2020-08-23 01:05:22 -0500

    exercise in Array

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -11,23 +11,26 @@ public class ArraysExercises {
         person[2] = new Person("Tristan");
 
         for( Person n: person){
-            System.out.println(n.getName());//getName?
+            System.out.println(n.getName());//in Person.java
         }
+
         System.out.println("Add a new person");
-        Person Trant = new Person("Trant");
+        Person trant = new Person("Trant");
         Person[] updatePeople = new Person[person.length+1];
-        updatePeople = addPerson(person,Trant);
+        updatePeople = addPerson(person,trant);
         System.out.println(("Printing out updated people array..."));
         for(Person n: updatePeople){
             System.out.println(n.getName());
         }
+
     }
 //        Create a static method named addPerson. It should accept an array of Person objects, as well as a single person object to add to the passed array. It should return an array whose length is 1 greater than the passed array, with the passed person object at the end of the array.
-        public static Person[] addPerson(Person[] existingPerson, Person newPeep){
-            Person[] updatePeeps = Arrays.copyOf(existingPerson, existingPerson.length+1);
-            updatePeeps[existingPerson.length]= newPeep;
-            return updatePeeps;
+        public static Person[] addPerson(Person[] existingPerson, Person newPerson){
+            Person[] addPerson = Arrays.copyOf(existingPerson, existingPerson.length+1);
+            addPerson[existingPerson.length] = newPerson;
+            return addPerson;
         }
+
 
 
 
