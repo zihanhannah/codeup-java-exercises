c533360e515d3b5cc482db2a0409f9e5e02b9597
zihan <zihanhannah@gmail.com>
2020-08-23 22:23:00 -0500

    exercise for Movie Array

---
+++ src/movies/Movie.java
@@ -0,0 +1,23 @@
+package movies;
+//Create a class named Movie. It should have private fields for name and category, and a constructor that sets both of these. Create methods to access these properties and change them (getters and setters).
+public class Movie {
+    private String name;
+    private String category;
+    public Movie(String name, String category){
+        this.name = name;
+        this.category = category;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public void setCategory(String category){
+        this.category = category;
+    }
+    public String getCategory(){
+        return category;
+    }
+}
