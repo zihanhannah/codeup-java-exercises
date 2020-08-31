debddc1fd4c270d52572b09c6780d3c5e793b901
Zihan Zhao <hannah@zihans-imac.lan>
2020-08-31 02:39:06 -0500

    done

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -4,10 +4,12 @@ import java.util.HashMap;
 
 public class GradesApplication {
     public static void main(String[] args) {
-        HashMap<String,String> students = new HashMap<>();
-        students.put("Ryan","ryanorsinger");
-        students.put("Zach","zgulde");
-        students.put("Fernando","fmendozaro");
-        students.put("Justin","jreich5");
+//        HashMap<String,String> students = new HashMap<>();
+//        students.put("Ryan","ryanorsinger");
+//        students.put("Zach","zgulde");
+//        students.put("Fernando","fmendozaro");
+//        students.put("Justin","jreich5");
+        Student student = new Student("hannah");
+        student.getGradeAverage();
     }
 }
--- src/shapes/Shape.java
+++ src/shapes/Shape.java
@@ -1,5 +1,5 @@
 package shapes;
 
 public abstract class Shape {
-
+//empty
 }
